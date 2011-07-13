c6f566c9767436a4d90d9bdf5c2e43ce6093553b
Chris Williams <chris@bingosnet.co.uk>
2011-07-13 11:20:37 +0000

    Import of BINGOS/Acme-POE-Knee-1.12 from CPAN.
    gitpan-cpan-distribution: Acme-POE-Knee
    gitpan-cpan-version:      1.12
    gitpan-cpan-path:         BINGOS/Acme-POE-Knee-1.12.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/Acme/POE/Knee.pm
+++ lib/Acme/POE/Knee.pm
@@ -1,9 +1,12 @@
 package Acme::POE::Knee;
+BEGIN {
+  $Acme::POE::Knee::VERSION = '1.12';
+}
+
+#ABSTRACT: Time sliced pony race using the POE kernel.
+
 use strict;
 use POE;
-use vars qw($VERSION);
-
-$VERSION = "1.10";
 
 sub new {
     my $class   = shift;
@@ -13,12 +16,12 @@ sub new {
         dist    => 10,
         ponies  => {
             'dngor'     => 5,
-            'Abigail'   => 5.2, 
-            'Co-Kane'   => 5.4, 
+            'Abigail'   => 5.2,
+            'Co-Kane'   => 5.4,
             'MJD'       => 5.6,
-            'acme'      => 5.8, 
+            'acme'      => 5.8,
         },
-    };  
+    };
 
     ### check for wrong input ###
     for my $key ( keys %args ) {
@@ -44,16 +47,16 @@ sub new {
     sub ponies  { my $self = shift; $self->{ponies}   }
 
     return $self;
-    
+
 }
 
 sub _start {
     my ($kernel, $heap, $name, $delay, $dist) = @_[KERNEL, HEAP, ARG0, ARG1, ARG2];
-    
+
     $heap->{name}   = $name;
     $heap->{delay}  = $delay;
     $heap->{dist}   = $dist;
-    
+
     printf "Starting pony %10s\n", $heap->{name};
     $kernel->delay_add( run => rand($heap->{delay}) );
 }
@@ -70,8 +73,8 @@ sub race {
 
     for my $name (@{[keys %{$self->ponies()}]} ) {
 
-        POE::Session->create (  
-           inline_states => { 
+        POE::Session->create (
+           inline_states => {
                _start  => \&_start,
                run     => \&run,
             },
@@ -81,20 +84,19 @@ sub race {
     $poe_kernel->run();
 }
 
-1;
+q[POE::Knee!];
+
 
 __END__
+=pod
 
 =head1 NAME
 
 Acme::POE::Knee - Time sliced pony race using the POE kernel.
 
+=head1 VERSION
 
-=head1 REQUIREMENTS
-
-    Acme::POE::Knee requires the POE module to run. You can get that as well
-    from CPAN or look at poe.sourceforge.net
-
+version 1.12
 
 =head1 SYNOPSIS
 
@@ -104,39 +106,29 @@ Acme::POE::Knee - Time sliced pony race using the POE kernel.
     # Use POEny!
     use Acme::POE::Knee;
 
-    # Every Acme::POE::Knee race will require a set of arguments. 
-    # There are defaults but it's just more fun to set these 
-    # yourselves. We set a distance the ponies must run and of course 
-    # we name our race ponies! You'll have to specify the maximum 
+    # Every Acme::POE::Knee race will require a set of arguments.
+    # There are defaults but it's just more fun to set these
+    # yourselves. We set a distance the ponies must run and of course
+    # we name our race ponies! You'll have to specify the maximum
     # delay a pony can have before reaching the next stage.
-    # The lower the delay, the higher the chances are the pony will 
+    # The lower the delay, the higher the chances are the pony will
     # win the race.
 
     my $pony = new Acme::POE::Knee (
-    	dist        => 20,
+	      dist   => 20,
         ponies  => {
             'dngor'     => 5,
-            'Abigail'   => 5.2, 
-            'Co-Kane'   => 5.4, 
+            'Abigail'   => 5.2,
+            'Co-Kane'   => 5.4,
             'MJD'       => 5.6,
-            'acme'      => 5.8, 
+            'acme'      => 5.8,
         },
     );
-    
+
     # start the race
     $pony->race( );
-    
+
     exit;
-
-
-=head1 QUICK LINKS
-
-Please see the samples directory in POE's distribution for several
-well-commented sample and tutorial programs.
-
-Please see <http://www.perl.com/pub/2001/01/poe.html> for an excellent,
-and more importantly: gradual, introduction to POE.
-
 
 =head1 DESCRIPTION
 
@@ -154,6 +146,19 @@ Of course this wouldn't be any fun if we couldn't name the ponies
 ourselves. Here, we simply put all our race ponies in an array
 reference and the Acme::POE::Knee module will take care of the rest.
 
+=for Pod::Coverage   new
+  race
+  dist
+  ponies
+  run
+
+=head1 QUICK LINKS
+
+Please see the samples directory in POE's distribution for several
+well-commented sample and tutorial programs.
+
+Please see <http://www.perl.com/pub/2001/01/poe.html> for an excellent,
+and more importantly: gradual, introduction to POE.
 
 =head1 USING Acme::POE::Knee
 
@@ -170,17 +175,15 @@ This simple progam would already suffice:
 This will use the defaults of the POE::Knee module, but you can of
 course specify your own arguments, as shown in the synopsis.
 
-
 =head1 The Use of Acme::POE::Knee
 
-Use, yes... Usefull? Probably not. This was written in responce to a
+Use, yes... Usefull? Probably not. This was written in response to a
 rather persistant meme on #perl (you know who you are!).
 Basicly, we all wanted ponies.
 Well folks, here it is.
 
-It's source might be interesting to look at for newcomers to POE to see
+Its source might be interesting to look at for newcomers to POE to see
 how this time slicing works.
-
 
 =head1 Learning more about POE
 
@@ -203,37 +206,28 @@ All forms of feedback are welcome.
 POE has a web site where the latest development snapshot, along with
 the Changes file and other stuff may be found: <http://poe.perl.org/>
 
-=item SourceForge
+=back
 
-POE's development has moved to SourceForge as an experiment in project
-management.  You can reach POE's project summary page at
-<http://sourceforge.net/projects/poe/>.
+=head1 AUTHORS
+
+=over 4
+
+=item *
+
+Jos Boumans <kane@cpan.org>
+
+=item *
+
+Rocco Caputo <rcaputo@cpan.org>
 
 =back
 
-=head2 Author
+=head1 COPYRIGHT AND LICENSE
 
-=over 2
+This software is copyright (c) 2011 by Jos Boumans.
 
-=item Jos Boumans
-
-Jos Boumans is <kane_at_cpan.org>.  POE::Knee is his brainchild.
-
-=item Rocco Caputo
-
-Rocco Caputo is <troc+poe@netrus.net>.  POE itself is his creation.
-
-=head2 COPYRIGHT
-
-Copyright (c) 2001, Jos Boumans. All Rights Reserved. This module
-is free software. It may be used, redistributed and/or modified
-under the terms of the Perl Artistic License (see
-http://www.perl.com/perl/misc/Artistic.html)
-
-Except where otherwise noted, POE is Copyright 1998-2001 Rocco Caputo.
-All rights reserved.  POE is free software; you may redistribute it
-and/or modify it under the same terms as Perl itself.
-
-=back
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
+
